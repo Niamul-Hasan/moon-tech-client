b0e211ce3edf95c11b5707f88900f4c5cd119d7c
Niamul Hasan Zead <zead.ece2014@gmail.com>
2022-12-05 20:09:40 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import { RouterProvider } from "react-router-dom";
 import routes from "./routes/Routes";
 
 function App() {
 
-  const [products, setProducts] = useState([]);
-  useEffect(() => {
-    fetch('http://localhost:5000/products')
-      .then(res => res.json())
-      .then(data => setProducts(data.data))
-  }, [])
+  // const [products, setProducts] = useState([]);
+  // useEffect(() => {
+  //   fetch('http://localhost:5000/products')
+  //     .then(res => res.json())
+  //     .then(data => setProducts(data.data))
+  // }, [])
   return (
     <div className="">
       <RouterProvider router={routes} />
