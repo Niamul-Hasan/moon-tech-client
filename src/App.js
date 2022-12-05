db5f70e5a04a0e1f74530bba0a28bfcba382e282
Niamul Hasan Zead <zead.ece2014@gmail.com>
2022-12-05 22:44:07 +0600

    commited

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-// import { useEffect, useState } from "react";
 import { RouterProvider } from "react-router-dom";
-import routes from "./routes/Routes";
+import routes from "./routes/Routes"
+import ProductProvider from "./ContextApi/ProductProvider";
 
 function App() {
-
-  // const [products, setProducts] = useState([]);
-  // useEffect(() => {
-  //   fetch('http://localhost:5000/products')
-  //     .then(res => res.json())
-  //     .then(data => setProducts(data.data))
-  // }, [])
   return (
-    <div className="">
-      <RouterProvider router={routes} />
+    <div>
+      <ProductProvider>
+        <RouterProvider router={routes} />
+      </ProductProvider>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React from "react";
+import ProductCart from "../Components/ProductCart";
+import { useProducts } from "../ContextApi/ProductProvider";
+
 
 const Home = () => {
+    const {
+        state: { products, loading, error },
+    } = useProducts();
+
+    let content;
+
+    if (loading) {
+        content = <p>Loading</p>;
+    }
+
+    if (error) {
+        content = <p>Something went wrong</p>;
+    }
+
+    if (!loading && !error && products.length === 0) {
+        content = <p>Nothing to show, product list is empty</p>;
+    }
+
+    if (!loading && !error && products.length) {
+        content = products.map((product) => (
+            <ProductCart key={product._id} product={product} />
+        ));
+    }
+
     return (
-        <div>
-            <h1 className='text-2xl text-teal-600'>This is home</h1>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
+            {content}
         </div>
     );
 };
